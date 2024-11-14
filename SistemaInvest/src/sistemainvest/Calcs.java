719a932a82f07025e3641fa5db79cbe4460fb1cf
Henrique <hcduarte200@gmail.com>
2024-11-13 21:45:11 -0300

    Correção Class Calcs

--- SistemaInvest/src/sistemainvest/Calcs.java
+++ SistemaInvest/src/sistemainvest/Calcs.java
@@ -1,36 +1,58 @@
 package sistemainvest;
 import java.util.Random;
 
+
+class ResultadoSimulacao {
+    public double investimentoFinal;
+    public double variacao;
+    public int months;
+    
+
+    public ResultadoSimulacao(double investimentoFinal, double variacao, int months) {
+        this.investimentoFinal = investimentoFinal;
+        this.variacao = variacao;
+        this.months = months;
+    }
+}
 public class Calcs {
 
-    public static double variacao;
-    static Random gerador = new Random();
-    static double porcentagem[] = new double[objeto.meses]; //cria um vetor do tamanho de meses. Preciso saber onde vai estar a variavel meses para completar
-    //TODO : Arrumar as gerações de número, tá estranho
-    public static void option1(float meses, double acresmensal, double invest) {
-        variacao = gerador.nextDouble() * 7 + 4; //gera um valor entre 0 e 1, mas nunca exibindo o 1
-        variacao = 1 + (variacao / 1000);
-        porcentagem[objeto.meses - 1] = variacao;
-        invest = invest + acresmensal;
-        invest = invest * variacao;
-        objeto.meses--;
+    public static double variation;
+    static Random generator = new Random();
+    static double percentage[] = new double[10]; //ainda n tenho ideia em q classe esse atributo ficaria
+
+    public static ResultadoSimulacao option1(int months, double monthlyIncrease, double investment) {
+        variation = 4 + generator.nextDouble() * 6; // valor minimo: 4 (6*0=0+4=4) valor max: 10(6*1=6+4=10)
+        variation = 1 + (variation / 1000);
+        percentage[(int)months] = variation;
+        investment = investment + monthlyIncrease;
+        investment = investment * variation;
+        months--;
+        
+        return new ResultadoSimulacao(investment, variation, months);
     }
 
-    public static void option2(int meses, double acresmensal, double invest) {
-        variacao = gerador.nextDouble() * 21;
-        variacao = 1 + (variacao / 1000);
-        porcentagem[objeto.meses - 1] = variacao;
-        invest = invest + acresmensal;
-        invest = invest + variacao;
-        objeto.meses--;
+    public static ResultadoSimulacao option2(int months, double monthlyIncrease, double investment) {
+        variation = generator.nextDouble() * 20; //min: 0  max: 20
+        variation = 1 + (variation / 1000);
+        percentage[(int)months] = variation;
+        investment = investment + monthlyIncrease;
+        investment = investment + variation;
+
+        months--;
+        
+        return new ResultadoSimulacao(investment, variation, months);
+
     }
 
-    public static void option3(int meses, double acresmensal, double invest) {
-        variacao = gerador.nextDouble() * 201 - 90;
-        variacao = 1 + (variacao / 1000);
-        porcentagem[objeto.meses - 1] = variacao;
-        invest = invest + acresmensal;
-        invest = invest + variacao;
-        objeto.meses--;
+    public static ResultadoSimulacao option3(int months, double monthlyIncrease, double investment) {
+        variation = -90 + generator.nextDouble() * 200; // min -90  max: 110 
+        variation = 1 + (variation / 1000);
+        percentage[(int)months] = variation;
+        investment = investment + monthlyIncrease;
+        investment = investment + variation;
+        
+        months--;
+        
+        return new ResultadoSimulacao(investment, variation, months);
     }
 }
